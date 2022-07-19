1086ee4625801dc9040b0424fd96ebf24a34ea0e
Aidan Horn <ahorn11@depaul.edu>
2022-07-19 13:22:56 -0700

    Added function for saving data to the python data file

--- PythonHub.py
+++ PythonHub.py
@@ -1,6 +1,7 @@
 import os
-
+pythonDataFile = ""
 #create the file meant to remember the python files
+
 def CreatePythonData():
 
     directorypath = os.getcwd()
@@ -16,9 +17,34 @@ def CreatePythonData():
     newfile = open(filepath,'a')
     
 
-
+    
     newfile.close()
+    pythonDataFile = filepath
+    AppendPythonData(pythonDataFile)
 
+    
+# add files to the data file  
+def AppendPythonData(pythonDataFile):
+    data = pythonDataFile
 
-
+    if data != "":
+        newfile = open(data,'a')
+        print("Type exit to stop")
+        while True:
+            
+            filename = str(input("Name of file meant to save (put whole path): "))
+            if filename != "exit":
+                newfile.write(filename+"\n")
+                
+                description = str(input("What is the description of the file? "))
+                if description != "exit":
+                    newfile.write(description+"\n")
+                else:
+                    break
+            else:
+                break
+            
+        newfile.close()
+            
 CreatePythonData()
+#AppendPythonData(pythonDataFile)
